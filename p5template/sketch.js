0b1becfee4a9938c49c87b5384d3649738bf96cf
Tom Keene <tom@theanthillsocial.co.uk>
2024-01-11 17:05:41 +0000

    Added template

--- p5template/sketch.js
+++ p5template/sketch.js
@@ -1,17 +1,26 @@
-
-let x = 0;
+let graph = [10, 50, 300, 60, 100]; 
+let x = 20;
 
 function setup() {
   createCanvas(500, 500); // canvas width/height
   background(0); // Make background black
+  // Generate an array of data
+  let values = 40; 
+  for(i=0;i<=values;i++){
+    graph[i] = random(5, height)
+  }
 }
 
 function draw(){
-  // Start - draw a white ellipse
-  fill(255)
-  rect(x, height/2, 20); // x, y, w, h
-  x += 5;
-  if(x>width){
-    x = 0;
+  fill(255);
+  // Display the data in a graph
+  for(i=0;i<graph.length;i++){
+    x = (11*i)+25;
+    createBar(x, graph[i]); 
   }
+}
+
+function createBar(x, h){
+  // Make sure the graph displays from the bottom
+  rect(x, height-h, 10, h); // x, y, w/h
 }
