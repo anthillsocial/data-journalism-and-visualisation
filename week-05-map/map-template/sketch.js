5363c2295f2755eeb9781a7aa61c8c4071d56d0f
Tom Keene <tom@theanthillsocial.co.uk>
2024-03-18 11:02:26 +0000

    Added mapping template

---
+++ week-05-map/map-template/sketch.js
@@ -0,0 +1,13 @@
+//adapted from Mappa tutorial
+//for more, see: https://mappa.js.org/docs/simple-map.html
+
+
+
+function setup() {
+  // Create our canvas element, and store its reference 
+  canvas = createCanvas(640, 800);
+}
+
+function draw(){
+
+}
