22883835545d4c9a5be76bbf484ed77c3929dce0
Tom Keene <tom@theanthillsocial.co.uk>
2025-02-20 17:03:10 +0000

    Added maps example

---
+++ week06-maps/menu.js
@@ -0,0 +1,8 @@
+// A consistent menu for each page
+function addMenu(){
+    let menu = document.getElementById("menu");
+    menu.innerHTML = `
+    <a href="index.html">Home</a> /
+    <a href="description.html">Description</a>
+    `;
+}
